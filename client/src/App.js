4179c780d60a63ac1538146ddad874cbf953ef10
Aayush-1999 <aayushaggarwal2007@gmail.com>
2020-05-23 03:33:02 +0530

    Add autChech functions and other routes in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,58 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom'
+import { Route , Redirect,Switch } from 'react-router-dom'
 import Home from './Containers/Home/Home'
 import Login from "./Containers/Login/Login";
 import Register from './Containers/Register/Register'
+import Books from './Containers/Books/Book'
+import Orders from './Containers/Orders/Orders'
 import Layout from './HOC/Layout/Layout'
-
-
+import {connect} from 'react-redux'
+import * as actions from './Store/Actions/Index'
 
 class App extends Component {
+
+  componentDidMount(){
+    this.props.onAuthCheck()
+  }
+
   render() {
-    return (
-      // <div>
-        <Layout>        
+    let routes=(
+      <Switch>
+        <Route path='/' exact component={Home} />
+        <Route path='/login' exact component={Login} />
+        <Route path='/register' exact component={Register} />
+        <Redirect to="/" />
+      </Switch>
+    )
+
+    if(this.props.isAuth){
+      routes=(
+        <Switch>
           <Route path='/' exact component={Home} />
-          <Route path='/login' exact component={Login} />
-          <Route path='/register' exact component={Register} />
-        </Layout>
-        // </div>
+          <Route path='/books' exact component={Books} />
+          <Route path='/orders' exact component={Orders} />
+          <Redirect to="/" />
+        </Switch>
+      )
+    }
+    return (
+      <Layout auth={this.props.isAuth}>
+        {routes}
+      </Layout>
     );
   }
 }
 
-export default App;
+const mapDispatchToProps=(dispatch)=>{
+  return{
+    onAuthCheck:()=>dispatch(actions.checkAuth())
+  }
+}
+
+const mapStateToProps=(state)=>{
+  return{
+    isAuth: state.auth.token!=null
+  }
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(App);
