71a26c655dab61ce5ea6d9a19fe3c7b688970770
Aayush-1999 <aayushaggarwal2007@gmail.com>
2020-05-23 03:34:17 +0530

    Convert functional into class component

--- client/src/HOC/Layout/Layout.js
+++ client/src/HOC/Layout/Layout.js
@@ -1,19 +1,19 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Aux from '../Auxiliary/Auxiliary'
 import Toolbar from '../../Components/Navigation/Toolbar/Toolbar'
 import classes from './Layout.module.css'
 
-const Layout = (props) => {
-    return (
-        <Aux className = {classes.body}>
-            <div>
-                <Toolbar />
-            </div>
-            <main >
-                {props.children}
-            </main>
-        </Aux>
-    )
+class Layout extends Component{
+    render(){
+        return (
+            <Aux className = {classes.body}>
+                <Toolbar isAuth={this.props.auth} />
+                <main >
+                    {this.props.children}
+                </main>
+            </Aux>
+        )
+    }
 }
 
 export default Layout
