d9ce9c48bbfe874bf7d001904e8f8070d18784e7
Aayush-1999 <aayushaggarwal2007@gmail.com>
2020-05-23 20:17:48 +0530

    create orders container

---
+++ client/src/Containers/Orders/Orders.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+class Orders extends Component{
+    render(){
+        return(
+            <h1>My orders</h1>
+        )
+    }
+}
+
+export default Orders
