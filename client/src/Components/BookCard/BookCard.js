71c85532681da8856a350a250445fdcb4bf13a98
Aayush-1999 <aayushaggarwal2007@gmail.com>
2020-05-23 20:18:47 +0530

    update bookcard

--- client/src/Components/BookCard/BookCard.js
+++ client/src/Components/BookCard/BookCard.js
@@ -1,21 +1,24 @@
-import React from 'react'
-import classes from './BookCard.module.css'
+import React from 'react';
+import classes from './BookCard.module.css';
 
 const BookCard=(props)=>{
     return(
         <div className={classes.card}>
-            <div className={classes.top-section}>
-                <img id="image-container" 
+            <div className={classes.topSection}>
+                <img className={classes.imageContainer} 
                 src="https://image.freepik.com/free-photo/feather-texture-background-with-orange-light_32525-101.jpg" />
                 <div className={classes.price}>Rs. {props.price}</div>
             </div>
-            <div className={classes.product-info}>
+            <div className={classes.productInfo}>
                 <div className={classes.name}>{props.title}</div>
                 <div className={classes.dis}>{props.author}</div>
-                <button className={classes.btn} onClick={props.addToCart} >ADD TO CART</button>
+                {props.ordered?
+                <button className={classes.btn} >ADDED TO CART</button>
+                :<button className={classes.btn} onClick={props.addToCart} >ADD TO CART</button>
+            }
             </div>
         </div>
     )
 }
 
-export default BookCard
+export default BookCard;
